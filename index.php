031405389feb8dabf7ce71f14be987a0a810e391
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-09-07 13:49:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<html>
-<body>
-
-<form action="welcome_get.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-
-</body>
+<?php require_once("header_php"); ?>
+  <h1>My website</h1>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
 </html>
