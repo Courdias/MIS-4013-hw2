c118536e25b9f7fc23a7c72a2d1f6f7e7773d5bd
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-09-07 14:19:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
     <html>
 <body>
 
-<form action="welcome.php" method="post">
+<form action="handlepost.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
