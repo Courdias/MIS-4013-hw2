290e674b9b1c29c021a8e2600e55855555d34d02
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-09-19 11:21:35 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -15,11 +15,7 @@
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 
-</body>
-</html>
-    <a class="btn btn-primary" href="
-                                     index.php" role="button">Go Home</a>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
+<?php
+  include "gohomebutton.php";
+?>
 </html>
