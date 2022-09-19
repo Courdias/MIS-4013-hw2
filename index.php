81b5361a53faddc4393fcaced78dd1e30b721a33
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-09-19 10:26:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ E-mail: <input type="text" name="email"><br>
 </body>
 </html>
     <a class="btn btn-primary" href="
-                                     page3.html" role="button">Go to Page 3</a>
+                                     header.php" role="button">Testing Header</a>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
