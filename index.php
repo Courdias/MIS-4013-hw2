33d928473958e89ab3bca8f25852612c2d32692b
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-09-19 11:32:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
-//this is my test index to make sure everything worked before breaking it down into sections
 <!doctype html>
 <?php
   include "header.php";
 ?>
    
-    <a class="btn btn-primary" href="
-                                     indexsections.php" role="button">Index, with Sections</a>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
 <?php
   include "dropdown.php";
 ?>
