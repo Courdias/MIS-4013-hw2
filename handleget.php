569e2232016634cb98cd313cfeb9e9eb034a6546
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-09-19 11:21:33 -0500

    Update handleget.php

--- handleget.php
+++ handleget.php
@@ -14,11 +14,8 @@
 Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
 
-</body>
-</html>
-    <a class="btn btn-primary" href="
-                                     index.php" role="button">Go Home</a>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
+<?php
+  include "gohomebutton.php";
+?>
+  
 </html>
