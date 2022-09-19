cf3352e7102f1d6150eb1b9f5dfed4822fd5656d
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-09-19 11:14:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+ 
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -71,5 +72,7 @@ E-mail: <input type="text" name="email"><br>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
+   <?php
   include "dropdown.php";
+  ?>
 </html>
