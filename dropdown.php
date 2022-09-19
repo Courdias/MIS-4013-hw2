d3e9c29c4b0814ef4ea1bb5bfbf0871da0ed59b6
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-09-19 11:11:42 -0500

    Create dropdown.php

---
+++ dropdown.php
@@ -0,0 +1,10 @@
+<div class="dropdown">
+  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown link
+  </a>
+
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="postinputpage.php">Go to Post Input Page</a></li>
+    <li><a class="dropdown-item" href="getinputpage.php">Go to Get Input Page</a></li>
+  </ul>
+</div>
