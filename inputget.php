5b8cb90bb85f19e5308a4297047721d4ff8e4ded
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-10-01 14:04:21 -0500

    Update inputget.php

--- inputget.php
+++ inputget.php
@@ -1,9 +1,5 @@
-    <html>
-<body>
 <form action="handleget.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
-  
-
