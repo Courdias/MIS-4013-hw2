cd077bd36b28aecd466e24ff65f03aea8093c82b
Nghia Bui <78659446+Courdias@users.noreply.github.com>
2022-10-01 14:13:49 -0500

    Update handleget.php

--- handleget.php
+++ handleget.php
@@ -8,7 +8,6 @@
   </div>
 </div>
     <h1>HW2 Get Response</h1>
-    <html>
 <body>
 
 Welcome <?php echo $_GET["name"]; ?><br>
@@ -17,12 +16,14 @@ Your email address is: <?php echo $_GET["email"]; ?>
 <?php
   include "gohomebutton.php";
 ?>
+  
   <?php
-function writeMsg() {
+function writeMsg() 
+{
   echo "You can tell this is a get because the input shows in the url!";
 }
 
 writeMsg();
 ?>
-
+<body>
 </html>
